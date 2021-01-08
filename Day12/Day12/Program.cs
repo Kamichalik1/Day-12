294942ede9d057095e7302ca8c33c9b424dffe20
Kamichalik1 <41804283+Kamichalik1@users.noreply.github.com>
2021-01-08 07:27:59 +0000

    Code Added

---
+++ Day12/Day12/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Part 2
+            int WaypointX = 10, WaypointY = 1;
+            int ShipX = 0, ShipY = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"E:\Advent Code\Day 12\Data.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Substring(0,1) == "N")
+                {
+                    WaypointY += Convert.ToInt32(lines[i].Substring(1));
+                }
+                else if (lines[i].Substring(0, 1) == "S")
+                {
+                    WaypointY -= Convert.ToInt32(lines[i].Substring(1));
+                }
+                else if (lines[i].Substring(0, 1) == "E")
+                {
+                    WaypointX += Convert.ToInt32(lines[i].Substring(1));
+                }
+                else if (lines[i].Substring(0, 1) == "W")
+                {
+                    WaypointX -= Convert.ToInt32(lines[i].Substring(1));
+                }
+                else if (lines[i].Substring(0, 1) == "R")
+                {
+                    double Theta = -Convert.ToInt32(lines[i].Substring(1))* Math.PI / 180;
+                    int TempWaypointX = (int)Math.Round( WaypointX * Math.Cos(Theta) - WaypointY * Math.Sin(Theta));
+                    WaypointY = (int)Math.Round(WaypointX * Math.Sin(Theta) + WaypointY * Math.Cos(Theta));
+                    WaypointX = TempWaypointX;
+                }
+                else if (lines[i].Substring(0, 1) == "L")
+                {
+                    double Theta = Convert.ToInt32(lines[i].Substring(1)) * Math.PI / 180;
+                    int TempWaypointX = (int)Math.Round(WaypointX * Math.Cos(Theta) - WaypointY * Math.Sin(Theta));
+                    WaypointY = (int)Math.Round(WaypointX * Math.Sin(Theta) + WaypointY * Math.Cos(Theta));
+                    WaypointX = TempWaypointX;
+                }
+                else if (lines[i].Substring(0, 1) == "F")
+                {
+                    int LoopCount = Convert.ToInt32(lines[i].Substring(1));
+
+                    ShipX += WaypointX * LoopCount;
+                    ShipY += WaypointY * LoopCount;
+                    
+                }
+               
+            }
+
+            Console.WriteLine(Math.Abs(ShipX)+Math.Abs(ShipY));
+            Console.ReadLine();
+        }
+    }
+}
